ef1d28e870f2c3d3331a7e9ebd444e230fb43758
David Morrisroe <pizzadave108@gmail.com>
2015-03-27 23:29:32 +0000

    apps4gaps hand up

---
+++ js/backbone.js
@@ -0,0 +1,15 @@
+// backbone.js info about backbone of site
+
+//var HTMLcreateForm = '<form id="%%data%%"></form>'
+//var HTMLformAction = '<form id="%%data%%" action=
+var HTMLcreateDropDown = '<div class ="drop-data drop-down%%data%%"></div>';
+var HTMLaddSelect = '<select class="select-data-box" id="%%data%%"></select>';
+var HTMLaddOption = '<option id="%%id%%" value="%%value%%">%%text%%</option>';
+var HTMLfirstOption = '<option selected="true" style="display:none;">%%display%%</option>';
+
+//var HTMLsubmit = '<input id="%%data%%" type="submit" onclick="showData()" value="View Data">';
+var HTMLbutton = '<button id="%%data%%" type="button" onclick="%%action%%()">%%display%%</button>';
+
+var HTMLgraphDiv = '<div id=graph-id%%data%% class ="graph"></div>';
+var HTMLgraphTitle = '<h3 class="main-title">%%main%%</h3>';
+var HTMLmapTitle = '<h3 class="map-title">%%main%%</h3>';
